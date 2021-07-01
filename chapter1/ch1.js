23670a7a3cd8ce213183278c6e67543e8718afa4
kywsc <fireyw@gmail.com>
2021-07-01 12:06:49 +0900

    중간 데이터  구조로 변경

--- chapter1/ch1.js
+++ chapter1/ch1.js
@@ -27,13 +27,16 @@ const invoices =
 console.log(statement(invoices, plays));
 
 function statement(invoice, plays) {
-    return renderPlainText(invoice, plays);
+    const statementData = {};
+    statementData.customer=invoice.customer;
+    statementData.performances=invoice.performances;
+    return renderPlainText(statementData, invoice, plays);
 }
 
-function renderPlainText(invoice, plays) {
-    let result = `청구내역( 고객명: ${invoice.customer})\n`;
+function renderPlainText(data, invoice, plays) {
+    let result = `청구내역( 고객명: ${data.customer})\n`;
 
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
         //청구 내역을 출시한다
         result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석) \n`;
     }
@@ -43,7 +46,7 @@ function renderPlainText(invoice, plays) {
 
     function totalAmount() {
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             //포인트를 적립한다
             result += amountFor(perf)
         }
@@ -53,7 +56,7 @@ function renderPlainText(invoice, plays) {
 
     function totalVolumeCredits() {
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             //포인트를 적립한다
             result += volumeCreditsFor(perf)
         }
