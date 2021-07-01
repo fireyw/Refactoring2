dc5ad3c40987cd97c16dd0bae735d2a12286b073
kywsc <fireyw@gmail.com>
2021-07-01 18:59:29 +0900

    amount, volumnCredits Create in sub class

--- chapter1/ch1.js
+++ chapter1/ch1.js
@@ -31,45 +31,43 @@ class PerformanceCalculator {
     }
 
     get amount() {
-        let result = 0; //변수 초기화
-        switch (this.play.type) {
-            case "tragedy": //비극
-                result = 40000;
-                if (this.performance.audience > 30) {
-                    result += 1000 * (this.performance.audience - 30)
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (this.performance.audience > 20) {
-                    result += 10000 + 500 * (this.performance.audience - 20)
-                }
-                result += 300 * this.performance.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르: ${this.performance.play.type}`);
-        }
-        return result;
+        throw new Error('서브클래스에서 처리하도록 설계되었습니다');
     }
 
     get volumeCredits() {
-        let volumeCredits = 0;
-        volumeCredits += Math.max(this.performance.audience - 30, 0);
-        //희극 관객 5명마다 추가 포인트를 제공한다
-        if ('comedy' === this.play.type) volumeCredits += Math.floor(this.performance.audience / 5);
-        return volumeCredits;
+        return Math.max(this.performance.audience - 30, 0);
     }
 }
 
-class TragedyCalculator extends PerformanceCalculator{
-
+class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30)
+        }
+        return result;
+    }
 }
 
-class ComedyCalculator extends PerformanceCalculator{
-
+class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 30000; //변수 초기화
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20)
+        }
+        result += 300 * this.performance.audience;
+        return result;
+    }
+    get volumeCredits() {
+        return super.volumeCredits+Math.floor(this.performance.audience / 5);
+    }
 }
 
-function createPerformanceCalculator(aPerformance, aPlay){
+function createPerformanceCalculator(aPerformance, aPlay) {
+    switch(aPlay.type){
+        case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+        case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+    }
     return new PerformanceCalculator(aPerformance, aPlay);
 }
 
